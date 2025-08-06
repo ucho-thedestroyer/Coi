8dcde1f8c33ff9c2636395764b2ccd5e541ded85
ucho-thedestroyer <204959246+ucho-thedestroyer@users.noreply.github.com>
2025-08-06 19:07:52 +0800

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -12,17 +12,6 @@ function addToQueue(song) {
   queue.appendChild(li);
 }
 
-function selectSong(songUrl) {
-  const audio = document.getElementById("audio-player");
-  audio.src = songUrl;
-  audio.play();
-
-  const queueList = document.getElementById("queue-list");
-  const li = document.createElement("li");
-  li.textContent = songUrl.split("/").pop();
-  queueList.appendChild(li);
-}
-
 function showTipOptions() {
   document.getElementById("tip-options").classList.remove("hidden");
 }
